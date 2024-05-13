46a5aabea3351aff1f050657186991918aa595c7
Soomlir <Soomlir@yandex.ru>
2024-05-13 21:58:15 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todo-list">
+    <form action="/">
+      <input type="text" name="text" :value="taskValue" />
+      <button @click="addTask" type="submit">Добавить</button>
+    </form>
+
+    <ol class="todo-list">
+      <li class="todo-list__item">Купить продуктов</li>
+      <li class="todo-list__item">Заплатить за интернет</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    addTask(event) {
+      console.log(event);
+    }
+  },
+};
+</script>
+
+
+<style scoped></style>
