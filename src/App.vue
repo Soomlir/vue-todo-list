deb71853cc68bb9fb9252d65a6a89db047789a16
Soomlir <Soomlir@yandex.ru>
2024-05-14 22:52:56 +0300

    base styles add

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="todo-list">
-    <form action="/">
-      <input type="text" name="text" :value="taskValue" />
-      <button @click="addTask" type="submit">Добавить</button>
+  <div class="todo">
+    <h1 class="todo__title">Todo list</h1>
+    <form class="todo__form" action="/">
+      <input class="todo__input" type="text" name="text" :value="taskValue" />
+      <select>
+        <option value="all">All</option>
+        <option value="complete">Complete</option>
+        <option value="incomplete">Incomplete</option>
+      </select>
     </form>
 
-    <ol class="todo-list">
+    <button></button>
+
+    <ol class="todo__list">
       <li class="todo-list__item">Купить продуктов</li>
       <li class="todo-list__item">Заплатить за интернет</li>
     </ol>
@@ -27,4 +34,34 @@ export default {
 </script>
 
 
-<style scoped></style>
+<style scoped>
+.todo {
+  display: grid;
+  grid-template-columns: 595px 85px 38px;
+  width: 750px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.todo__title {
+  grid-column: 1 / -1;
+  font-size: 26px;
+  line-height: 30px;
+  text-transform: uppercase;
+  color: #252525;
+  text-align: center;
+}
+
+.todo__form {
+  grid-column: 1 / 3;
+  display: flex;
+}
+
+.todo__input {
+  min-width: 595px;
+}
+
+.todo__list {
+  grid-column: 1 / -1;
+}
+</style>
